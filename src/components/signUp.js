791a3809bd7f06e86cf97960b38ab9692b8a10a0
José Menezes <jose.menezes@petronect.com.br>
2020-10-19 15:45:50 -0300

    Custom signUp page and submit

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,41 +1,144 @@
 import React, {useState} from 'react'
-import {View,Text,Button,TextInput} from 'react-native'
+import {
+    View,
+    Text,
+    Button,
+    TextInput,
+    StyleSheet,
+    TouchableOpacity,
+    Dimensions,
+    Alert
+} from 'react-native'
+import Amplify from 'aws-amplify';
+import {Auth} from 'aws-amplify'
+import {validateEmail,validatePassword} from '../validation'
+import awsconfig from '../../aws-exports';
+
+Amplify.configure(awsconfig);
+
+const windowWidth = Dimensions.get('window').width;
 
 export default function SignUp(props){
+    
     const [state, setState] = useState({
         email: '',
         password: '',
     });
+    const [error,setErrors] = useState({
+        email:'',password:''
+    });
+
+    async function onSubmit() {
+        const emailError = validateEmail(state.email);
+        const passwordError = validatePassword(state.password);
+        if(emailError || passwordError) {
+            setErrors({email:emailError,password:passwordError});
+        }else{
+            try{
+                console.log({
+                    username: state.email,
+                    password: state.password,
+                })
+                const user = await Auth.signUp({
+                    username: state.email,
+                    password: state.password,
+                });
+            }catch(errorMsg){
+                Alert.alert(errorMsg);
+            }
+        }
+    }
 
     if(props.authState === 'signUp'){
         return (
-            <View>
-                <Text>SignUp</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>SignUp</Text>
+                <Text style={styles.label}>Email:</Text>
                 <TextInput
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                onChangeText={(text) => setState({...state, email: text})}
-                value={''}
+                style={styles.input}
+                onChangeText={(text) => setState({...state, email: text.toLowerCase()})}
+                placeholder="Seu email"
+                value={state.email}
                 />
+                <Text style={styles.error}>{error.email}</Text>
+                <Text style={styles.label}>Senha:</Text>
                 <TextInput
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                style={styles.input}
                 onChangeText={(text) => setState({...state, password: text})}
-                value={''}
+                placeholder="Sua senha"
+                value={state.password}
                 />
-                <Button
-                onPress={() => props.onStateChange('signIn', {})}
-                title="Voltar para Login"
-                color="#841584"
-                accessibilityLabel="Voltar para Login"
-                />
-                <Button
-                onPress={() => props.onStateChange('confirmSignUp', {})}
-                title="Confirmar código"
-                color="#841584"
-                accessibilityLabel="Confirmar código"
-                />
+                <Text style={styles.error}>{error.password}</Text>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onSubmit()}>
+                    <Text style={styles.buttonText}>Press Here</Text>
+                </TouchableOpacity>
+                <View style={styles.links}>
+                    <Button
+                    onPress={() => props.onStateChange('signIn', {})}
+                    title="Voltar para Login"
+                    color="black"
+                    accessibilityLabel="Voltar para Login"
+                    />
+                    <Button
+                    onPress={() => props.onStateChange('confirmSignUp', {})}
+                    title="Confirmar código"
+                    color="black"
+                    accessibilityLabel="Confirmar código"
+                    />
+                </View>
             </View>
         )
     }else{
         return <></>
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        height:'100%',
+        justifyContent : 'center',
+        width : windowWidth,
+        padding: 20,
+    },
+    button: {
+        backgroundColor : '#4682B4',
+        height : 40,
+        borderRadius : 5,
+        justifyContent:'center',
+    },
+    buttonText: {
+        textTransform: 'uppercase',
+        color:'white',
+        textAlign:'center',
+    },
+    input: { 
+        height: 40, 
+        borderColor: 'lightgray', 
+        borderWidth: 1,
+        marginBottom: 5,
+        borderRadius: 5,
+        padding: 10,
+    },
+    links:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: '600',
+        textTransform: 'uppercase',
+    },
+    label: {
+        marginLeft: 5,
+        marginBottom: 5,
+    },
+    error: {
+        color: 'red',
+        paddingBottom: 10,
+        marginLeft: 5,
+    },
+  });
