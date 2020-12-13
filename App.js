888db1012fada101af6c92b2ddb0f835d2b87406
José Menezes <jcarlos78@gmail.com>
2020-12-13 19:33:53 -0300

    main app.js with custom screens

--- App.js
+++ App.js
@@ -3,51 +3,41 @@ import {Text, View, StyleSheet} from 'react-native';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import {Authenticator} from 'aws-amplify-react-native'
-import { withAuthenticator,
-  SignIn, 
-  ConfirmSignUp,
-  ConfirmSignIn,
-  ForgotPassword, 
+import {Auth} from 'aws-amplify'
+import {
+  ConfirmSignIn, 
+  // ForgotPassword,
 } from 'aws-amplify-react-native';
-import SignUp from './src/components/signUp'
-
-
-// const signUpConfig = {
-//   header: 'Sat Editor',
-//   hideAllDefaults: true,
-//   defaultCountryCode: 55,
-//   signUpFields: [
-//     {
-//       label: 'Email',
-//       key: 'email',
-//       required: true,
-//       displayOrder: 1,
-//       type: 'string'
-//     },
-//     {
-//       label: 'Senha',
-//       key: 'password',
-//       required: true,
-//       displayOrder: 1,
-//       type: 'password'
-//     }
-//   ]
-// };
+import SignUp from './src/components/SignUp'
+import ConfirmSignUp from './src/components/ConfirmSignUp'
+import SignIn from './src/components/SignIn'
+import ForgotPassword from './src/components/ForgotPassword'
 
 const usernameAttributes = 'email';
-
-
-//export default withAuthenticator(App, {signUpConfig,usernameAttributes})
 
 Amplify.configure(awsconfig);
 
 function Home(props){
   if(props.authState === 'signedIn'){
-    return <Text>Home</Text>;
+    return <Text onPress={signOut}>Sign Out</Text>;
   } else {
     return <></>
   } 
+
+  function signOut() {
+    Auth.signOut()
+      .then(() => {
+        props.onStateChange('signedOut', null);
+      })
+      .catch(err => {
+        console.log('err: ', err)
+      })
+  }
+
 }
+
+
+
 
 export default function App() {
   return (
